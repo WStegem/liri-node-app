a1ba95ceff693bf9387608a1c894949bfc6f68bb
unknown <wstegem@utexas.edu>
2018-12-11 20:49:26 -0600

    Bandsintown functionality complete

--- liri.js
+++ liri.js
@@ -2,21 +2,28 @@ require("dotenv").config();
 var fs = require('fs')
 var file = 'random.txt'
 var keys = require('./keys')
+var axios = require('axios')
+var moment = require('moment');
 
 function concert(artist){
-    console.log('concert', artist)
+    console.log('Searching for concert:', artist+'\n')
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+        .then(function(response){
+            
+            console.log('The next '+artist+' concert will be:\nVenue: '+response.data[0].venue.name+'\nLocation: '+response.data[0].venue.city+', '+response.data[0].venue.region+', '+response.data[0].venue.country+'\nDate: '+moment(response.data[0].datetime).format('L'))
+        })
 }
 function song(songName){
-    console.log('song', songName)
+    console.log('Searching for song:', songName)
 }
 function movie(movieName){
-    console.log('movie', movieName)
+    console.log('Searching for movie:', movieName)
 }
 
 function rand(){
     fs.readFile(file, 'utf8', function(error, data){
         if(error){
-            return console.log('and... death');
+            return console.log('This file has been corrupted: \n'+error);
         }
         var list = data.split(',').map(x => x.trim());
         if(list[0] == 'concert-this'){
